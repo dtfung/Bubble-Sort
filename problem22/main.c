ad7732f1ac09d890ee6d56581e47beec747d04dd
Donald Fung <donaldtfung@gmail.com>
2015-09-13 18:33:47 -0400

    Bubble Sort
    Sorting an array using Bubble Sort

---
+++ problem22/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  problem22
+//
+//  Created by Aditya Narayan on 5/20/15.
+//  Copyright (c) 2015 Aditya Narayan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int myArray[6] = {2,1,5,3,4};
+    char temp;
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5 - 1; j++) {
+            
+            if (myArray[j] > myArray[j + 1]) {
+                 temp = myArray[j];
+                myArray[j] = myArray[j + 1];
+                myArray[j + 1] = temp;
+            }
+        }
+   }
+    for (int i = 0; i < 5; i ++) {
+          printf("%i", myArray[i]);
+    }
+  
+    return 0;
+}
